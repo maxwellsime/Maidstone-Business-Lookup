14ffad66914baf429c485d7a73cf97a708c43d2d
mjsime <maxwellsime@gmail.com>
2021-08-17 18:06:57 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+Maxwell Sime 2020
+
+main.html
+==========
+Webpage containing jQuery script using AJAX
+Creates table and populates it with business data from (now unaccessible) server-side scripts "hygeine.php" and "ratings.php"
---
+++ hygiene.php
@@ -0,0 +1,30 @@
+[{"business":"14
+ Forty FM Services - Compass Group","address":"Nhs Supplies, 20\/20 
+Industrial Estate, St Barnabas Close, Maidstone, ME16 
+0LW","type":"Restaurant\/Cafe\/Canteen","rating":"5","date":"2019-03-13"},{"business":"4Degrees","address":"9
+ - 10 Royal Star Arcade, High Street, Maidstone, Kent, ME14 
+1JL","type":"Restaurant\/Cafe\/Canteen","rating":"1","date":"2019-02-21"},{"business":"A
+ And Z Fisheries","address":"72 Linton Road, Loose, Maidstone, Kent, 
+ME15 0AT","type":"Takeaway\/sandwich 
+shop","rating":"5","date":"2018-08-09"},{"business":"AA Markets 
+Cafeteria","address":"Lockmeadow Cafe, Lockmeadow Market, Barker Road, 
+Maidstone, ME16 
+8RG","type":"Restaurant\/Cafe\/Canteen","rating":"3","date":"2018-06-06"},{"business":"Abbeyrose
+ Residential Home","address":"Abbeyrose House Residential Home, 1 St 
+Michaels Road, Maidstone, Kent, ME16 
+8BS","type":"Hospitals\/Childcare\/Caring 
+Premises","rating":"5","date":"2018-09-03"},{"business":"Absolutely 
+Ravenous","address":"Raglan House, St Peters Street, Maidstone, Kent, 
+ME16 0SN","type":"Other catering 
+premises","rating":"5","date":"2019-03-25"},{"business":"Age 
+Concern","address":"Rosemary Graham Day Centre, Somner Walk, Maidstone, 
+Kent, ME15 9PR","type":"Hospitals\/Childcare\/Caring 
+Premises","rating":"5","date":"2019-06-10"},{"business":"Age 
+UK","address":"Harbledown House, Fant Lane, Maidstone, Kent, ME16 
+8NZ","type":"Restaurant\/Cafe\/Canteen","rating":"5","date":"2019-06-24"},{"business":"Age
+ UK (Maidstone)","address":"Shepway Court, Norfolk Road, Maidstone, 
+Kent, ME15 
+7JF","type":"Restaurant\/Cafe\/Canteen","rating":"5","date":"2019-03-29"},{"business":"Age
+ UK Maidstone The Goodman Centre","address":"6 Egremont Road, Bearsted, 
+Maidstone, Kent, ME15 
+8LH","type":"Restaurant\/Cafe\/Canteen","rating":"5","date":"2018-12-12"}]
